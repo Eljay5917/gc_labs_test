396f96697fe050ef0948a85565330a899aaa4ee3
Eljay5917 <elijahmawuliattakorah@gmail.com>
2023-08-31 10:55:44 +0000

    thursday morning

--- project/app.py
+++ project/app.py
@@ -1,26 +1,39 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import requests 
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def reqeusts():
+
+
+import requests
+api_key="ee2798328aa7160fd1a37d3ad3a29b1a"
+url =f'http://api.openweathermap.org/data/2.5/weather?q=Koforidua&appid={api_key}'
+
+get_requests=requests.get(url)
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        country = request.form['country']
+        weather = request.form['weather']
+        return render_template('results.html', country=country, weather=weather)
     return render_template("index.html")
 
-#app.route('/', methods=['POST'])
-#def process_form():
-   # country = request.form['country']
-    #weather = request.form['weather']
-
-    # Process the data or perform any desired actions
-
-    
-    #return render_template('result_template.html', country=country, weather=weather)
-
-@app.route('/results')
+@app.route('/results', methods=['GET', 'POST'])
 def results():
-    return render_template("results.html")
+    if request.method == 'POST':
+        country = request.form['country']
+        weather = request.form['weather']
+        return render_template("results.html", country=country, weather=weather)
+    return "Invalid request"
+
 @app.errorhandler(404)
-def page_not_found():
-    return render_template("404.html"),404
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0')
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
